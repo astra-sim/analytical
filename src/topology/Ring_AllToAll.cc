75e58018c864d7830671076fbd69581fb778ea7a
rashidi1saeed <58501728+rashidi1saeed@users.noreply.github.com>
2020-11-06 23:13:49 -0500

    Update Ring_AllToAll.cc

--- src/topology/Ring_AllToAll.cc
+++ src/topology/Ring_AllToAll.cc
@@ -4,6 +4,7 @@ LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
 #include "Ring_AllToAll.hh"
+#include <assert.h>
 
 using namespace Analytical;
 
