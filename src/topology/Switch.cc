8e3b9353c831ca8f6e71138a0d57a5d4ac74560c
rashidi1saeed <58501728+rashidi1saeed@users.noreply.github.com>
2020-11-06 23:14:27 -0500

    Update Switch.cc

--- src/topology/Switch.cc
+++ src/topology/Switch.cc
@@ -4,6 +4,7 @@ LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
 #include "Switch.hh"
+#include <assert.h>
 
 using namespace Analytical;
 
