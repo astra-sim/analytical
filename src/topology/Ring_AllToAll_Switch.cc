41617e92c649fb975deaa91c14dae8da67731ff5
rashidi1saeed <58501728+rashidi1saeed@users.noreply.github.com>
2020-11-06 23:14:08 -0500

    Update Ring_AllToAll_Switch.cc

--- src/topology/Ring_AllToAll_Switch.cc
+++ src/topology/Ring_AllToAll_Switch.cc
@@ -4,6 +4,7 @@ LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
 #include "Ring_AllToAll_Switch.hh"
+#include <assert.h>
 
 using namespace Analytical;
 
