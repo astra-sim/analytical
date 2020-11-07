7174738a8a3a71fa1be6e602cc372ad2996b9f24
rashidi1saeed <58501728+rashidi1saeed@users.noreply.github.com>
2020-11-06 23:15:18 -0500

    Update TopologyConfiguration.cc

--- src/topology/TopologyConfiguration.cc
+++ src/topology/TopologyConfiguration.cc
@@ -4,6 +4,7 @@ LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
 #include "TopologyConfiguration.hh"
+#include <assert.h>
 
 using namespace Analytical;
 
